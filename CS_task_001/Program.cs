eec26f77f1b07214ec5d53b3d58780c50cfc4f41
rn4nv <dorjuan@ya.ru>
2022-12-21 03:27:03 +0700

    Completed CS task 001

---
+++ CS_task_001/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.Clear();
+
+Console.WriteLine("Заполните массив из 5 чисел: ");
+
+void FillArray(int[] collection)
+
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = int.Parse(Console.ReadLine());
+        index++;
+    }
+}
+
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.Write($"{col[position]} ");
+//         position++;
+//     }
+// }
+
+int NumbersNegative (int[] collection)
+{
+    int count = collection.Length;
+    int index = 0;
+    int result = 0;
+
+    while (index < count)
+    {
+        if (collection[index] < 0)
+        {
+            result++;
+            index++;
+        }
+        else
+        {
+            index++;
+        }
+    }
+    return result;
+}
+
+
+int[] Array = new int [5];
+FillArray(Array);
+Console.WriteLine();
+// PrintArray(Array);
+// Console.WriteLine();
+Console.WriteLine($"Количество найденных чисел в введенном массиве равно: {NumbersNegative(Array)}");
+
